b58d95442ed96c25066661981c642716781d6ae6
MKruger777 <morne.kruger@gmail.com>
2019-06-17 08:24:16 +0200

    Update from the test serevr

--- manaday.sh
+++ manaday.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-echo "Starting cmd a day!" 
+#set -x
+echo "Starting cmd a day!"
+echo "my log" >> /home/_mkruge19/dev/bash/manaday/logs/mylog
 echo ""
 echo "1: Checking the dependancies..."
 manlib='/home/_mkruge19/dev/bash/manaday/manlib'
@@ -9,7 +11,7 @@ manualsfound=$(ls -lR | grep ^d | wc -l)
 
 if [ "$manualsfound" -eq "0" ]
   then
-   	echo "No man folders found in manlib. These are required to continue - please investigate! Script will now exit."
+        echo "No man folders found in manlib. These are required to continue - please investigate! Script will now exit."
     exit
 fi
 
@@ -24,9 +26,9 @@ counter=0
 foldername=""
 foldernames=()
 for afolder in $folderlist; do
-	#echo $afolder
-	foldernames[$counter]=$afolder
-	#echo ${foldernames[$counter]}
+        #echo $afolder
+        foldernames[$counter]=$afolder
+        #echo ${foldernames[$counter]}
     let counter=counter+1
 done
 IFS=$'\n'
@@ -34,10 +36,10 @@ IFS=$'\n'
 
 if [ "${#foldernames[@]}" -le "0" ]
   then
-   	echo "No man folders found. Please investigate! Script will now exit."
+        echo "No man folders found. Please investigate! Script will now exit."
     exit
 else
-		echo "  > man folders found=${#foldernames[@]}. Good to go!"		
+                echo "  > man folders found=${#foldernames[@]}. Good to go!"
 fi
 
 
@@ -51,25 +53,26 @@ echo ""
 echo "3: starting man selection..."
 completed='/home/_mkruge19/dev/bash/manaday/completed'
 
-mansentcounter=$(cat $completed| wc -l)
+mansentcounter=$(cat $completed | wc -l)
 echo "Total commands sent (NOT including todays!)=" $mansentcounter
 
 if [ "$mansentcounter" -ge "$max" ]
   then
-   	echo "All manuals were sent. Resetting the completed file..."
-   	> completed
+        echo "All manuals were sent. Resetting the completed file..."
+        > completed
 fi
 
 while true; do
-	#generate random number between the smallest and largest number...
-	genid=$(shuf -i $min-$max -n 1)
-	echo "The man id for today is: $genid"
+        #generate random number between the smallest and largest number...
+        genid=$(shuf -i $min-$max -n 1)
+        echo "The man id for today is: $genid"
+        echo "$genid" >> /home/_mkruge19/dev/bash/manaday/logs/mylog
 
-	if grep -Fxq $genid $completed
-	then
-	    echo "  > this was allready done - generate a new number..."
-	else
-	    echo "  > This was not yet done - send the mail!"
+        if grep -Fxq $genid $completed
+        then
+            echo "  > this was allready done - generate a new number..."
+        else
+            echo "  > This was not yet done - send the mail!"
 
         while read recipient; do
           echo "sending mail to: $recipient"
@@ -79,24 +82,23 @@ while true; do
           #echo $manpath
           #manpath='/home/_mkruge19/dev/bash/mail/0/cp.man'
           recipients="$recipient"
-          subject='Manaual of the day is :  '$(awk '/NAME/{getline; print}' $manpath | xargs)
+          subject= 'Manaual of the day is :  '$(awk '/NAME/{getline; print}' $manpath | xargs)
           echo "subject is $subject"
           from="postman"
           message=$(cat $manpath )
 
           /usr/sbin/sendmail "$recipients" <<EOF
-          subject:$subject"bla"
-          from:$from
-          $message
-          $subject
+Subject: $subject
+From: $from
+$message
 EOF
 
         done < /home/_mkruge19/dev/bash/manaday/recipients
 
 
-	    #insert the manid that was sent into the completed list 
-	    echo $genid  >> $completed
-	    echo "3: done"
-	    break
-	fi
+            #insert the manid that was sent into the completed list
+            echo $genid  >> $completed
+            echo "3: done"
+            break
+        fi
 done
